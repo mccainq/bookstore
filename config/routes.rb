adc84103cd8d6e49412feeb8a82233adf4832a39
Quentin McCain <qmccain@gmail.com>
2015-04-16 13:16:44 -0400

    Class 04 - Lab 7 - pagination

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :books
+  resources :books do
+    get 'page/:page', :action => :index, :on => :collection
+  end
+
   root 'books#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
