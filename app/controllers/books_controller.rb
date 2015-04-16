9ddeddf83b489a5f944bcc965d8f48ad90b97264
Quentin McCain <qmccain@gmail.com>
2015-04-15 20:48:59 -0400

    Class 04 - Lab 1&2 - validation/show errors

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,18 +14,23 @@ class BooksController < ApplicationController
   end
 
   def create
-    logger.debug "#{params}"
     @book = Book.new(book_params)
-    @book.save
-    redirect_to @book
+    if @book.save
+      redirect_to @book
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
-    @book.update(book_params)
-    redirect_to @book
+    if @book.update(book_params)
+      redirect_to @book
+    else
+      render :new
+    end
   end
 
   def destroy
