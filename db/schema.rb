3fb35abda5aa97be3f4767709b282a83397a22a9
Quentin McCain <qmccain@gmail.com>
2015-04-22 14:52:03 -0400

    Generated review model and related to book

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150402010456) do
+ActiveRecord::Schema.define(version: 20150422184343) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -21,5 +21,16 @@ ActiveRecord::Schema.define(version: 20150402010456) do
     t.datetime "updated_at", null: false
     t.decimal  "price"
   end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "stars"
+    t.text     "comment"
+    t.integer  "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "reviews", ["book_id"], name: "index_reviews_on_book_id"
 
 end
